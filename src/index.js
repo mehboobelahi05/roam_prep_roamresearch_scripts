f9578ed34a550d9599c997c98c3f6a65113b250c
Mehboob Elahi <mehboobelahi@MacBookAir.lan>
2026-06-18 01:01:35 +0530

    Auto-update bundle

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import { testFunction, test_var } from './test.js';
+import { testFunction, test_var, elahi } from './test.js';
 import { abcFunction } from './abc.js';
 
 // Bundle everything into a single global namespace for Roam
 window.RoamPrep = {
     test_var: test_var,
     testFunction: testFunction,
-    abcFunction: abcFunction
+    abcFunction: abcFunction,
+    elahi: elahi
 };
 
 console.log("🚀 Roam Prep Project Engine fully loaded!");
--- src/test.js
+++ src/test.js
@@ -2,3 +2,4 @@ export function testFunction() {
     console.log("Test function from the bundled project executed!");
 }
 export const test_var = "hello ddddddd";
+export const elahi = "Mehboob Elahi";
