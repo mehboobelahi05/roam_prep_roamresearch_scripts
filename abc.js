3c4002a1c52058d8371544155a5c67e7a54fa84c
Mehboob Elahi <mehboobelahi@MacBookAir.lan>
2026-06-17 23:05:34 +0530

    Updated test script features

---
+++ abc.js
@@ -0,0 +1,11 @@
+// Keep your working variable
+var abc_def = "hello";
+
+// Option A: Write the function as a var expression (forces it global)
+var abcFunction = function() {
+    console.log("This is a test function working via var!");
+};
+
+
+// Add this line so you can see in real-time when the script refreshes
+console.log("ABC Script updated and loaded perfectly!");
